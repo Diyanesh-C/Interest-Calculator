7fcb8e3aea7b8567a0119ba764932ebb46b36891
MonikaS3 <monika.s3@cognizant.com>
2025-05-30 09:35:24 +0530

    Validating the EMI calculation for given input is completed ticketno - 13

---
+++ cypress/e2e/CarLoan.cy.js
@@ -0,0 +1,28 @@
+import CarLoan from "../support/Pages/CarLoan";
+
+describe('',()=>{
+    let url;
+    let loanAmount;
+    let interest;
+    let tenure;
+  beforeEach(()=>{
+    Cypress.on('uncaught:exception',()=> false);
+  })
+
+  before('',()=>{
+    cy.fixture('example').then((data)=>{
+        url = data.url;
+        loanAmount= data.loanAmount;
+        interest = data.interest;
+        tenure =  data.tenure;
+    })
+  })
+
+  it('',()=>{
+    CarLoan.visit(url);
+  })
+
+  it('',()=>{
+    CarLoan.validinputs(loanAmount,interest,tenure);
+  })
+})
