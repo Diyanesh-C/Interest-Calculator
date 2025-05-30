e5ebf87165e99a2c617c018919518510b454414f
Diyanesh-C <diyanesh.p@cognizant.com>
2025-05-30 11:17:28 +0530

    updated the ticketno - 68

---
+++ cypress/e2e/TenureSpec.cy.js
@@ -0,0 +1,15 @@
+import TenurePage from '../support/Pages/TenureValidation';
+
+const Tenure = new TenurePage();
+
+describe('Car Loan Tenure', () => {
+  beforeEach(()=>{
+    Tenure.visit(); 
+  })
+  it('Negative Boundary', () => {
+    Tenure.boundaryCheck();
+})
+Cypress.on("uncaught:exception",(err,runnable)=>{
+  return false;
+})
+})
