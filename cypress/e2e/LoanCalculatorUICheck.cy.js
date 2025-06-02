80c1f9f3f2252b7518f855dd295ca9cd6ebf1e28
deepakkumaravelu-c <deepak.kumaravelu@cognizant.com>
2025-06-02 09:37:12 +0530

    verification of slider and input sync completed tickeno - 76

--- cypress/e2e/LoanCalculatorUICheck.cy.js
+++ cypress/e2e/LoanCalculatorUICheck.cy.js
@@ -3,11 +3,16 @@ import UIValidation from "../support/Pages/UIValidation";
 const ui = new UIValidation();
  
 describe('Loan EMI Calculator UI Validation via Menu', () => {
-  it('should validate presence and usability of Loan, Interest, and Tenure inputs and sliders', () => {
+  it.skip('should validate presence and usability of Loan, Interest, and Tenure inputs and sliders', () => {
     ui.visitHomePage();
     ui.navigateToLoanCalculator();
     ui.verifyUrl();
     ui.verifyLoanInputsAndSliders();
+  });
+  it('should reflect changes between input fields and sliders in both directions', () => {
+    ui.visitHomePage();
+    ui.navigateToLoanCalculator();
+    ui.verifyBiDirectionalSync();
   });
   Cypress.on("uncaught:exception",(err,runnable)=>{
     return false;
