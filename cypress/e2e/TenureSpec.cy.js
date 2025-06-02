b81573c10b14fcd600a1faff9ca00bb2909234ae
Diyanesh-C <diyanesh.p@cognizant.com>
2025-06-02 21:29:27 +0530

    Non-Numberic Value Validation

--- cypress/e2e/TenureSpec.cy.js
+++ cypress/e2e/TenureSpec.cy.js
@@ -3,14 +3,21 @@ import TenurePage from '../support/Pages/TenureValidation';
 const Tenure = new TenurePage();
 
 describe('Car Loan Tenure', () => {
+    
   beforeEach(()=>{
     Tenure.visit(); 
   })
+
   it('Boundary Check', () => {
     Tenure.boundaryCheck();
   })
+
   it('Empty Value Check',()=>{
     Tenure.emptinessCheck();
+  })
+
+  it('Non-Numberic Value Validation',()=>{
+    Tenure.nonNumericValidation();
   })
 Cypress.on("uncaught:exception",(err,runnable)=>{
   return false;
