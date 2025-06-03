08c0f3195924554226a0e7f7f101978466a7309e
deepakkumaravelu-c <deepak.kumaravelu@cognizant.com>
2025-06-03 08:35:01 +0530

    updated

--- cypress/e2e/LoanCalculatorUICheck.cy.js
+++ cypress/e2e/LoanCalculatorUICheck.cy.js
@@ -3,13 +3,13 @@ import UIValidation from "../support/Pages/UIValidation";
 const ui = new UIValidation();
  
 describe('Loan EMI Calculator UI Validation via Menu', () => {
-  it.skip('should validate presence and usability of Loan, Interest, and Tenure inputs and sliders', () => {
+  it('should validate presence and usability of Loan, Interest, and Tenure inputs and sliders', () => {
     ui.visitHomePage();
     ui.navigateToLoanCalculator();
     ui.verifyUrl();
     ui.verifyLoanInputsAndSliders();
   });
-  it.skip('should reflect changes between input fields and sliders in both directions', () => {
+  it('should reflect changes between input fields and sliders in both directions', () => {
     ui.visitHomePage();
     ui.navigateToLoanCalculator();
     ui.verifyBiDirectionalSync();
