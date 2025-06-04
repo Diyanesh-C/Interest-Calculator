eca563f500b37615b970d5c199ffa2a3372b7203
L G <2400317@cognizant.com>
2025-06-04 18:21:22 +0530

    spec names updated

--- cypress/e2e/spec.cy.js
+++
@@ -1,9 +0,0 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://emicalculator.net/');
-    cy.log("visit");
-  })
-  Cypress.on("uncaught:exception",(err,runnable)=>{
-    return false;
-  })
-})
